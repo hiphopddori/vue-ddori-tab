6d5582531836a8cd0555e29267dd40da2a45953d
ddori <6290071+hiphopddori@users.noreply.github.com>
2020-01-14 10:27:31 +0900

    Update Demo.vue

--- src/components/Demo.vue
+++ src/components/Demo.vue
@@ -1,12 +1,12 @@
 <template>
     <dr-tabs>
-        <dr-tab :name="'Tab1'" :selected=true>
+        <dr-tab name="Tab'" selected="true">
             TAB1
         </dr-tab>
-        <dr-tab :name="'Tab2'">
+        <dr-tab name="Tab2">
             <span>TAB2</span>
         </dr-tab>
-        <dr-tab :name="'Tab3'">
+        <dr-tab name="Tab3">
             <span>TAB3</span>
         </dr-tab>
     </dr-tabs>
